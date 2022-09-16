0d4aa38c1493cb3025b75aa55ef44b1812aa2cc1
kjw009 <kjw007@icloud.com>
2022-09-16 18:08:40 +0100

    Implemented fetch for /pages

--- ui/src/Pages.js
+++ ui/src/Pages.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+// /Pages component
 const Pages = () => {
-    return <div>Documents:</div>
-}
+  // Assign JSON to state variable
+  const [data, setData] = useState([]);
+
+  // Fetch JSON data from backend /pages endpoint on the first render
+  useEffect(() => {
+    fetch("/pages").then((res) =>
+      res.json().then((data) => {
+        setData(data.titles);
+        console.log(data);
+      })
+    );
+  }, []);
+
+  // Return
+  return (
+    <>
+    <h1>Documents:</h1>
+    <ul>
+      {data.map((title) => (
+        <h3>{title}</h3>
+      ))}
+    </ul>
+    </>
+  );
+};
 
 export default Pages;
