c3cf75332e950459811c170e6178f46b1e325b19
kjw009 <kjw007@icloud.com>
2022-09-16 18:57:17 +0100

    Hello component fetches data from an api

--- ui/src/Hello.js
+++ ui/src/Hello.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Hello = () => {
-    return <div>Hello</div>
-}
+  // Store JSON data using state variable
+  const [data, setData] = useState("");
+  const [title, setTitle] = useState("");
+
+  // Fetch latest revision of Hello document on first render
+  useEffect(() => {
+    fetch("/page/hello/latest").then((res) =>
+      res.json().then((data) => {
+        setData(data.data);
+        setTitle(data.title);
+        console.log(data);
+      })
+    );
+  }, []);
+
+  //Return
+  return (
+    <>
+      <h1>
+        <u>{title}</u>
+      </h1>
+      <p>{data}</p>
+    </>
+  );
+};
 
 export default Hello;
