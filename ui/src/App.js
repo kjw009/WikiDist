dcf71ebff594b706ff6a37a4de17eef74c586261
kjw009 <kjw007@icloud.com>
2022-09-16 17:51:40 +0100

    Implemented router dom

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,10 +1,22 @@
+// Import react modules
 import React from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+
+// Import functional components
 import Pages from "./Pages";
+import Hello from "./Hello";
+import Passfort from "./Passfort";
 
 function App() {
   return (
-    <Pages />
-  )
+    <BrowserRouter>
+    <Switch>
+      <Route path="/pages" component={Pages} />
+      <Route path="/page/hello" component={Hello} />
+      <Route path="/page/passfort" component={Passfort} />
+    </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ ui/src/Passfort.js
@@ -0,0 +1,10 @@
+// Import react
+import React from "react";
+
+// Passfort document component
+const Passfort = () => {
+    // Return
+    return <div>Passfort</div>
+}
+
+export default Passfort;
